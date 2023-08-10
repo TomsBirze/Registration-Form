11d11d5ece4a99b7e07918b0fc6fc62928619953
TomsBirze <toms.birze@inbox.lv>
2023-08-10 23:56:25 +0300

    Added axios to send registration to server. Added succes or failur anotation

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,9 +1,36 @@
-import $ from 'jquery';
-import sum from './utils/sum/sum';
+import axios from "axios";
 
-console.log('Ready for coding');
 
-console.log('Body jQuery node:', $('body'));
-console.log('Body javascript node:', document.querySelector('body'));
-console.log('2 + 3 =', sum(2, 3));
+const form = document.querySelector<HTMLFormElement>('.js-form')
 
+type DataToSend = { [key: string]: unknown }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    const finalData: DataToSend = {};
+
+    for (const pair of formData.entries()) {
+        finalData[pair[0]] = pair[1]
+    }
+    console.log(finalData)
+
+    axios.post('http://localhost:3004/registrations', Object.fromEntries(formData))
+        .then(() => {
+            displayMessage('Registration successful!', 'success');
+        })
+        .catch(() => {
+            displayMessage('Registration failed. Please try again.', 'error');
+        });
+  
+
+});
+
+function displayMessage(message: string, type: string) {
+    const messageBox = document.createElement('div');
+    messageBox.className = `message-box ${type}-message`;
+    messageBox.textContent = message;
+    form.appendChild(messageBox);
+}
